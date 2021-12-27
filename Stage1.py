044ce8ab4ccfd71a1a4b1c78e3c90feb7522994b
Flores <chamizo.flores@gmail.com>
2021-12-27 11:30:45 +0100

    Stage1 solved

--- Stage1.py
+++ Stage1.py
@@ -1,3 +1,11 @@
+
 if __name__ == '__main__':
     for i in range(1, 101):
-        print(i)
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
